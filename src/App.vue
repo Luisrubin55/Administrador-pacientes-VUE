16ed80cfcf119d3d9d50c44e1d33a02a7873ab17
Luis <luisrubin55@gmail.com>
2023-11-24 11:16:28 -0600

    Proyecto Terminado

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,96 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, reactive, onMounted, watch } from 'vue'
+import { uid } from 'uid'
+
+import Header from './components/Header.vue';
+import Formulario from './components/Formulario.vue'
+import Paciente from './components/Paciente.vue'
+
+const pacientes = ref([])
+
+const paciente = reactive({
+  id: null,
+  nombre: '',
+  propietario: '',
+  email: '',
+  alta: '',
+  sintomas: ''
+})
+
+watch(pacientes, () => {
+  guardarLocalStorage()
+}, {
+  deep: true
+})
+
+onMounted(() => {
+  const pacienteStorage = localStorage.getItem('pacientes')
+  if (pacienteStorage) {
+    pacientes.value = JSON.parse(pacienteStorage)
+  }
+})
+
+const guardarLocalStorage = () => {
+  localStorage.setItem('pacientes', JSON.stringify(pacientes.value))
+}
+
+
+
+const guardarPaciente = () => {
+  if (paciente.id) {
+    const { id } = paciente
+    const i = pacientes.value.findIndex((pacienteState) => pacienteState.id === id)
+    pacientes.value[i] = { ...paciente }
+  } else {
+    pacientes.value.push({
+      ...paciente,
+      id: uid()
+    })
+  }
+
+  //Reiniciar el objeto
+  paciente.nombre = ''
+  paciente.propietario = ''
+  paciente.email = ''
+  paciente.alta = ''
+  paciente.sintomas = ''
+  id: null
+}
+
+const actualizarPaciente = (id) => {
+  const pacienteEditar = pacientes.value.filter(paciente => paciente.id === id)[0]
+  Object.assign(paciente, pacienteEditar)
+}
+
+const eliminarPaciente = (id) => {
+  pacientes.value = pacientes.value.filter(paciente => paciente.id !== id)
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <div class="container mx-auto mt-20">
+    <Header />
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <div class="mt-12 md:flex">
+      <Formulario v-model:nombre="paciente.nombre" v-model:propietario="paciente.propietario"
+        v-model:email="paciente.email" v-model:alta="paciente.alta" v-model:sintomas="paciente.sintomas"
+        @guardar-paciente="guardarPaciente" :id="paciente.id" />
+
+      <div class="md:w-1/2 md:h-screen overflow-y-scroll ">
+        <h3 class="font-black text-3xl text-center">Administra tus Pacientes</h3>
+
+        <div v-if="pacientes.length > 0">
+          <p class="text-lg mt-5 text-center mb-10">
+            Información de
+            <span class="text-indigo-600 font-bold ">Pacientes</span>
+          </p>
+          <Paciente v-for="paciente in pacientes" :paciente="paciente" @actualizar-paciente="actualizarPaciente"
+            @eliminar-paciente="eliminarPaciente" />
+        </div>
+
+        <p v-else class="mt-14 text-2xl text-center">No hay pacientes aún</p>
+
+      </div>
+    </div>
+  </div>
+</template>
